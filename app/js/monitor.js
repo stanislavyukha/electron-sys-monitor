04f68dff1eecea200b8a4fbc533685cca58988f8
Stas Yukha <valhalla245@gmail.com>
2021-07-05 18:31:00 +0300

    added cpu load dynamic info

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -1,9 +1,37 @@
 const path = require('path');
 const osu = require('node-os-utils');
+const { inherits } = require('util');
 const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+let cpuOverload = 10;
+//run every 2sec
+
+setInterval(() => {
+    //Dynamic sys info
+    cpu.usage().then(info => {
+        document.getElementById('cpu-usage').textContent = info.toFixed(2) + '%';
+        document.getElementById('cpu-progress').style.width = info + '%';
+        //on cpu overload set bar red color
+        if(info > cpuOverload) {
+            document.getElementById('cpu-progress').style.backgroundColor = 'red';
+        } else {
+            document.getElementById('cpu-progress').style.backgroundColor = 'inherit';
+        }
+    });
+
+    cpu.free().then(info => {
+        document.getElementById('cpu-free').textContent = info.toFixed(2) + "%";
+    })
+
+    os.uptime()
+    document.getElementById('sys-uptime').textContent = secondsToDhms(os.uptime());
+
+},2000)
+
+
+//static sys info
 
 //set model
 document.getElementById('cpu-model').textContent = cpu.model();
@@ -20,3 +48,16 @@ mem.info().then(info => {
     document.getElementById('mem-total').textContent = info.totalMemMb;
 
 });
+
+//show human time
+
+function secondsToDhms(seconds) {
+    seconds = +seconds;
+
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor((seconds) % (3600 * 24) / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor(seconds % 60);
+
+    return `${d}d,${h}h,${m}m,${s}s`;
+}
